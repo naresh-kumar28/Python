04b22d0cf319f0a11aa73e42ec9d4e32467f21eb
Naresh Kumar <nareshkumarsah780@gmail.com>
2025-12-30 18:51:27 +0530

    new program

---
+++ Basic Program/11.py
@@ -0,0 +1,18 @@
+# Q-11. Dictionary Example
+
+# W.A.P to create a dictionary containing some personal details like name, city, state, and country.
+
+# Print the dictionary and its data type.
+
+x = {
+    "name" : "naresh",
+    "city" : "amour",
+    "contact" : 7070509875,
+    "country" : "india"
+}
+
+#print value
+print("value :",x)
+
+#print data type
+print(type(x))
